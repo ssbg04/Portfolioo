f2d503fd001d85d3d88cf8fb8c50152253f7b733
Cris Charles <143066036+ssbg04@users.noreply.github.com>
2026-07-20 22:45:13 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: 'hybrid',
   integrations: [react()],
 
   vite: {
